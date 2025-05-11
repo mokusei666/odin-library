68837c211cdc7ed543816021beaf69b036e03046
Cameron Brown <moontsukuyomi6@gmail.com>
2025-05-11 05:00:03 +0200

    Create a library book constructor function to allow user to create book

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(author, title, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+}
+
+const theHobbit = new Book('J.R.R Tolkien', 'The Hobbit', 368, 'No');
+console.log(theHobbit);
